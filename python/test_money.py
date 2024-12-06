f35554b0ffbb77be747374cd97f9b16a2856cfaf
Navaneeth <zorbhathebuddha@gmail.com>
2024-12-06 22:40:53 +0800

    feat: refactored to different modules

--- python/test_money.py
+++ python/test_money.py
@@ -1,19 +1,7 @@
 import unittest
 
-
-class Money:
-    def __init__(self, amount, currency) -> None:
-        self.amount = amount
-        self.currency = currency
-
-    def times(self, multiplier):
-        return Money(self.amount * multiplier, self.currency)
-
-    def divide(self, divisor):
-        return Money(self.amount / divisor, self.currency)
-
-    def __eq__(self, other) -> bool:
-        return self.amount == other.amount and self.currency == other.currency
+from money import Money
+from portfolio import Portfolio
 
 
 class TestMoney(unittest.TestCase):
@@ -28,11 +16,17 @@ class TestMoney(unittest.TestCase):
         self.assertEqual(twenty_euros, ten_euros.times(2))
 
     def test_division(self):
-        original_money = Money(4002, "KRW")
-        actual_money_after_division = original_money.divide(4)
-        expected_money_after_division = Money(1000.5, "KRW")
-        self.assertEqual(expected_money_after_division,
-                         actual_money_after_division)
+        four_thousand_two = Money(4002, "KRW")
+        one_thousand_point_five = four_thousand_two.divide(4)
+        self.assertEqual(one_thousand_point_five, four_thousand_two.divide(4))
+
+    def test_addition(self):
+        five_dollars = Money(5, "USD")
+        ten_dollars = Money(10, "USD")
+        fifteeen_dollars = Money(15, "USD")
+        portfolio = Portfolio()
+        portfolio.add(five_dollars, ten_dollars)
+        self.assertEqual(fifteeen_dollars, portfolio.evaluate("USD"))
 
 
 if __name__ == "__main__":
